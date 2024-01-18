a7a1f23008413c4c25c7b209cdd9c2d37b8226da
John Ibrahim <john.ibrahim119@gmail.com>
2024-01-18 15:13:07 -0500

    feat: finished milestone 1

--- mcmerch.py
+++ mcmerch.py
@@ -1,15 +1,20 @@
 import sys
 
 def main():
-    tax = {"ON": 0.13}
+    tax = {"ON": 0.13, "QC": 0.14975, "MB": 0.05, "NS": 0.15, "BC": 0.05}
     
+    # extract the first argument as price
     price = float(sys.argv[1])
 
+    # extract the second argument as quantity
     quantity = int(sys.argv[2])
+
+    # extract the third argument as province
+    province = sys.argv[3]
 
     pre_tax = price * quantity
 
-    after_tax = pre_tax * (1 + tax["ON"])
+    after_tax = pre_tax * (1 + tax[province])
 
     print(f"The total cost is: ${round(after_tax, 2)}")
 
